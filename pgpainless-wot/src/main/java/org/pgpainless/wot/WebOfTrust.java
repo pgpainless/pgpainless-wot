8373c1e447be10750949a6bf07558582e6d61223
Paul Schaub <vanitasvitae@fsfe.org>
2023-06-19 10:58:04 +0200

    We can once again compile"

--- pgpainless-wot/src/main/java/org/pgpainless/wot/WebOfTrust.java
+++ pgpainless-wot/src/main/java/org/pgpainless/wot/WebOfTrust.java
@@ -1,7 +1,7 @@
 package org.pgpainless.wot;
 
 import org.bouncycastle.openpgp.PGPPublicKeyRing;
-import pgp.certificate_store.Certificate;
+import pgp.certificate_store.certificate.Certificate;
 import pgp.certificate_store.exception.BadDataException;
 
 import java.io.IOException;
@@ -18,8 +18,8 @@ public class WebOfTrust implements CertificateAuthority {
      * Do the heavy lifting of calculating the web of trust.
      */
     public void initialize() throws BadDataException, IOException {
-        Certificate trustRoot = certificateStore.getTrustRoot();
-        
+        Certificate trustRoot = certificateStore.getTrustRootCertificate();
+        // TODO: Implement
     }
 
     @Override
