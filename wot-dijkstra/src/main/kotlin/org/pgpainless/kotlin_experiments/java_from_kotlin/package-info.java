c0f99459d2760f1fc96951b4d6040cb57c8f9467
Paul Schaub <vanitasvitae@fsfe.org>
2023-06-25 10:56:50 +0200

    Remove Kotlin experiments

--- wot-dijkstra/src/main/kotlin/org/pgpainless/kotlin_experiments/java_from_kotlin/package-info.java
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2023 Paul Schaub <vanitasvitae@fsfe.org>
-//
-// SPDX-License-Identifier: Apache-2.0
-
-/**
- * Demonstration package.
- * TODO: Delete later.
- */
-package org.pgpainless.kotlin_experiments.java_from_kotlin;
