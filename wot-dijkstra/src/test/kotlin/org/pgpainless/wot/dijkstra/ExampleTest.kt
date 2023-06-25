5c8ae08c1ddaa394bf40afdc3a12194ef84691f3
Paul Schaub <vanitasvitae@fsfe.org>
2023-06-25 23:42:54 +0200

    Add example kotlin test

---
+++ wot-dijkstra/src/test/kotlin/org/pgpainless/wot/dijkstra/ExampleTest.kt
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2023 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.pgpainless.wot.dijkstra
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+class ExampleTest {
+
+    @Test
+    fun testSomething() {
+        assertNotEquals("PGPainless", "GnuPG")
+        assertEquals(24, 14 + 10)
+    }
+}
