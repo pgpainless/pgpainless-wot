dd7cf4b6876380b2dfd6aedc032d66566d33d9ef
Paul Schaub <vanitasvitae@fsfe.org>
2023-07-03 15:52:02 +0200

    Rework FingerprintTest

--- wot-dijkstra/src/test/kotlin/org/pgpainless/wot/dijkstra/FingerprintTest.kt
+++ wot-dijkstra/src/test/kotlin/org/pgpainless/wot/dijkstra/FingerprintTest.kt
@@ -8,17 +8,23 @@ import kotlin.test.assertFalse
 class FingerprintTest {
 
     @Test
-    fun testConstructor() {
+    fun `verify that Fingerprint applies an uppercase() on the constructor argument`() {
+        val fromLowerCase = Fingerprint("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        assertEquals("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", fromLowerCase.fingerprint)
+    }
+
+
+    @Test
+    fun `verify that objects constructed from the lower- and uppercase representation do equal`() {
         val finger1 = Fingerprint("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
         val finger2 = Fingerprint("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
 
         assert(finger1 == finger2)
-        assertEquals("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", finger1.fingerprint)
         assertEquals(finger1.hashCode(), finger2.hashCode())
     }
 
     @Test
-    fun testEquals() {
+    fun `verify the proper function of the equals() method and == operator`() {
         val finger1 = Fingerprint("0000000000000000000000000000000000000000")
         val finger2 = Fingerprint("1111111111111111111111111111111111111111")
 
