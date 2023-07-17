d592bc2665e90cc3eec64757ee8631c7a8fab52b
Paul Schaub <vanitasvitae@fsfe.org>
2023-07-17 12:47:04 +0200

    Remove superfluous overriding t0 in inherited Cliques test

--- sequoia-wot-vectors/src/testFixtures/kotlin/org/sequoia_pgp/wot/vectors/CliquesLocalOptimaVectors.kt
+++ sequoia-wot-vectors/src/testFixtures/kotlin/org/sequoia_pgp/wot/vectors/CliquesLocalOptimaVectors.kt
@@ -6,11 +6,6 @@ package org.sequoia_pgp.wot.vectors
 
 class CliquesLocalOptimaVectors: CliquesVectors() {
 
-    /**
-     * A few minutes after the network is fully generated.
-     */
-    val t0 = parseReferenceTime("2021-02-14 00:00:00 UTC")
-
     override fun getResourceName(): String {
         return "org/sequoia_pgp/wot/vectors/cliques-local-optima.pgp"
     }
