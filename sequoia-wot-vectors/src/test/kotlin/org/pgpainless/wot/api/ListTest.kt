0563966ca970c046ace843f878620cac8453d0a8
Paul Schaub <vanitasvitae@fsfe.org>
2023-07-20 14:07:40 +0200

    Add test for the list API

---
+++ sequoia-wot-vectors/src/test/kotlin/org/pgpainless/wot/api/ListTest.kt
@@ -0,0 +1,43 @@
+// SPDX-FileCopyrightText: 2023 Paul Schaub <vanitasvitae@fsfe.org>
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+package org.pgpainless.wot.api
+
+import org.junit.jupiter.api.Test
+import org.pgpainless.PGPainless
+import org.pgpainless.wot.KeyRingCertificateStore
+import org.pgpainless.wot.WebOfTrust
+import org.pgpainless.wot.network.Root
+import org.pgpainless.wot.network.Roots
+import org.sequoia_pgp.wot.vectors.BestViaRootVectors
+import kotlin.test.assertEquals
+
+class ListTest {
+
+    @Test
+    fun `best-via-root - verify that we can list only the trust-root at t0`() {
+        val v = BestViaRootVectors()
+        val keyRing = PGPainless.readKeyRing().publicKeyRingCollection(v.keyRingInputStream())
+        val store = KeyRingCertificateStore(keyRing)
+        val network = WebOfTrust(store).buildNetwork(referenceTime = v.t0)
+
+        val roots = Roots(Root(v.aliceFpr))
+        val api = WoTAPI(network, roots, false, false, 120, v.t0)
+
+        assertEquals(1, api.list().bindings.size)
+    }
+
+    @Test
+    fun `best-via-root - verify that we can list all certificates at t1`() {
+        val v = BestViaRootVectors()
+        val keyRing = PGPainless.readKeyRing().publicKeyRingCollection(v.keyRingInputStream())
+        val store = KeyRingCertificateStore(keyRing)
+        val network = WebOfTrust(store).buildNetwork(referenceTime = v.t1)
+
+        val roots = Roots(Root(v.aliceFpr))
+        val api = WoTAPI(network, roots, false, false, 120, v.t1)
+
+        assertEquals(6, api.list().bindings.size)
+    }
+}
